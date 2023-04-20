d5d2ccb66fa125db6b48090fb1a61cfea239baef
youjin <123347724+youjinjoy@users.noreply.github.com>
2023-04-20 19:51:32 +0900

    Add functionality to assign functions as values to Object keys

--- src/index.js
+++ src/index.js
@@ -4,40 +4,6 @@ const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
 // <⚠️ /DONT DELETE THIS ⚠️>
 
 const title=document.querySelector("h2");
-
-function handleMouseClick(){
-    title.innerText="Mouse Click";
-    title.style.color=colors[0];
-}
-title.addEventListener("click",handleMouseClick);
-
-function handleMouseEnter(){
-    title.innerText="Mouse Enter";
-    title.style.color=colors[1];
-}
-title.addEventListener("mouseenter",handleMouseEnter);
-
-function handleMouseLeave(){
-    title.innerText="Mouse Leave";
-    title.style.color=colors[2];
-}
-title.addEventListener("mouseleave",handleMouseLeave);
-
-function handleWindowResize(){
-    title.innerText="Window Resize";
-    title.style.color=colors[3];
-}
-window.addEventListener("resize",handleWindowResize);
-// 주의: title에 이벤트 리스너를 주는게 아닌 window에 주어야.
-
-// 마우스 우클릭
-function handleContextMenu(){
-    title.innerText="Context Menu";
-    title.style.color=colors[4];
-}
-title.addEventListener("contextmenu",handleContextMenu);
-
-
 /*
 ✅ The text of the title should change when the mouse is on top of it.
 ✅ The text of the title should change when the mouse is leaves it.
@@ -47,5 +13,35 @@ title.addEventListener("contextmenu",handleContextMenu);
 ✅ DO NOT CHANGE .css, or .html files.
 ✅ ALL function handlers should be INSIDE of "superEventHandler"
 */
-const superEventHandler = {    
+
+// Object는 key에 대한 value로 함수를 가질 수 있다!
+const superEventHandler = {  
+    'handleMouseClick':()=>{
+        title.innerText="Mouse Click";
+        title.style.color=colors[0];
+    },
+    'handleMouseEnter':()=>{
+        title.innerText="Mouse Enter";
+        title.style.color=colors[1];
+    },
+    'handleMouseLeave':()=>{
+        title.innerText="Mouse Leave";
+        title.style.color=colors[2];
+    },   
+    'handleWindowResize':()=>{
+        title.innerText="Window Resize";
+        title.style.color=colors[3];
+    },
+    // 주의: title에 이벤트 리스너를 주는게 아닌 window에 주어야.
+    // 마우스 우클릭
+    'handleContextMenu':()=>{
+        title.innerText="Context Menu";
+        title.style.color=colors[4];
+    },
 };
+
+title.addEventListener("click",superEventHandler.handleMouseClick);
+title.addEventListener("mouseenter",superEventHandler.handleMouseEnter);
+title.addEventListener("mouseleave",superEventHandler.handleMouseLeave);
+window.addEventListener("resize",superEventHandler.handleWindowResize);
+title.addEventListener("contextmenu",superEventHandler.handleContextMenu);  
